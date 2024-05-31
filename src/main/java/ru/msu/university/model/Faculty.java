4f61270ecfb02d12af87d4b63120ac9bdcfe40a8
Mariia <mariia_merkel@mail.ru>
2024-05-31 11:24:59 +0300

    Добавила связь между стедентами и факультетами

--- src/main/java/ru/msu/university/model/Faculty.java
+++ src/main/java/ru/msu/university/model/Faculty.java
@@ -1,11 +1,9 @@
 package ru.msu.university.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Faculty {
@@ -15,6 +13,9 @@ public class Faculty {
     private Long id;
     private String name;
     private String color;
+
+    @OneToMany(mappedBy = "faculty")
+    private Set<Student> students;
 
     public Faculty() {
     }
