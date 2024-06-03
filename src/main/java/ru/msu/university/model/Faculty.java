c2f89af552f88f621c7ff0307e786c60f239712f
MariiaMerkel <mariia_merkel@mail.ru>
2024-06-03 08:46:21 +0300

    Поменяла стратегию генерации id для сущностей, предварительно удалив таблицы из бд, чтобы id шли попорядку

--- src/main/java/ru/msu/university/model/Faculty.java
+++ src/main/java/ru/msu/university/model/Faculty.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class Faculty {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String color;
--- src/main/java/ru/msu/university/model/Student.java
+++ src/main/java/ru/msu/university/model/Student.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
@@ -73,18 +73,20 @@ public class Student {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", age=" + age +
+                ", faculty=" + faculty +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Student student)) return false;
-        return id == student.id && age == student.age && Objects.equals(name, student.name);
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(faculty, student.faculty);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, age);
+        return Objects.hash(id, name, age, faculty);
     }
 }
