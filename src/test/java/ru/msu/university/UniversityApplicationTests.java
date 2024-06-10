62fa91e25edad5a0b85c553533e2f4377a92fece
MariiaMerkel <mariia_merkel@mail.ru>
2024-06-10 08:01:57 +0300

    написала тест на загрузку контроллера для студентов

--- src/test/java/ru/msu/university/UniversityApplicationTests.java
+++ src/test/java/ru/msu/university/UniversityApplicationTests.java
@@ -1,13 +1,28 @@
 package ru.msu.university;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.msu.university.controller.StudentController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UniversityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void contextLoads() throws Exception {
+        Assertions.assertThat(studentController).isNotNull();
+    }
 
 }
