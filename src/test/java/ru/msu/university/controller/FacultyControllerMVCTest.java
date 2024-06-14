cf9d3edea1e1f7267f47516d68dfb32beb48e9c6
MariiaMerkel <mariia_merkel@mail.ru>
2024-06-14 22:20:10 +0300

    Начала писать интеграционные тесты для факультетов

--- src/test/java/ru/msu/university/controller/FacultyControllerMVCTest.java
+++ src/test/java/ru/msu/university/controller/FacultyControllerMVCTest.java
@@ -1,2 +1,10 @@
-package ru.msu.university.controller;public class FacultyControllerMVCTest {
+package ru.msu.university.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@WebMvcTest
+@ActiveProfiles("home")
+public class FacultyControllerMVCTest {
+
 }
