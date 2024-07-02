61c0f239b7a9f58b64e572ee5ed5f2cd3e6ab9fd
MariiaMerkel <mariia_merkel@mail.ru>
2024-07-02 07:48:23 +0300

    задала ограничения и значения по умолчанию

--- src/scripts421.sql
+++ src/scripts421.sql
@@ -1,2 +1,8 @@
 ALTER TABLE students
-ADD CONSTRAINT age_constraint CHECK (age > 16);
+ADD CONSTRAINT age_constraint CHECK (age > 16),
+ADD CONSTRAINT name_unique UNIQUE (name),
+ALTER COLUMN name SET NOT NULL,
+ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE faculties
+ADD CONSTRAINT color_name_unique UNIQUE (color, name);
