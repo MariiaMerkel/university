4a0e2a301cdf3f74959b954255aec304e0558777
MariiaMerkel <mariia_merkel@mail.ru>
2024-07-13 10:10:56 +0300

    поправила миграцию и проперти для тестов

--- src/scripts.sql
+++
@@ -1,5 +0,0 @@
-select * from student where age between 10 and 40;
-select "name" from student;
-select * from student where "name" ~* 'm';
-select * from student where age < id;
-select * from student order by age;
